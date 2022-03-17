297b165549dedba1bffaa2dbf03561d220bb6651
Juan José Estrada <69777842+jjestrada2@users.noreply.github.com>
2022-03-17 10:09:47 -0700

    V1

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    // Write your code here
+ar.sort((a,b)=>a-b);
+var numberOfPairs=0;
+console.log(ar);
+
+for(let i=0;i<ar.length;i++){
+   var numberOfSocksOfOneColor=Math.floor((ar.lastIndexOf(ar[i])+1-i)/2);
+   console.log(numberOfSocksOfOneColor);
+   numberOfPairs=numberOfPairs+numberOfSocksOfOneColor;
+   i=ar.lastIndexOf(ar[i]);
+}
+return numberOfPairs;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
